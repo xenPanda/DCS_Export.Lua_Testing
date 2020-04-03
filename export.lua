04fa10de4b65ef9fe7b27efe77436b30a09f8af4
xenPanda <ctbarilla@gmail.com>
2020-04-03 10:31:13 -0700

    Update export.lua
    working export.lua

--- export.lua
+++ export.lua
@@ -1,56 +1,52 @@
+local log_file = nil
+
 function LuaExportStart()
 
-local PANDA = {}
 
   package.path = package.path..";"..lfs.currentdir().."/LuaSocket/?.lua"
   package.cpath = package.cpath..";"..lfs.currentdir().."/LuaSocket/?.dll"
 
-PANDA.RECEIVE_PORT = 9086
-PANDA.SEND_TO_PORT = 31090
 
-
-
--- local socket = require("socket")
--- PANDA.UDPSendSocket = socket.udp()
--- PANDA.UDPLosReceiveSocket = socket.udp()
-
--- local JSON = loadfile("Scripts\\JSON.lua")()
--- PANDA.JSON = JSON
-
--- socket.try(PANDA.UDPSendSocket:sendto(PANDA.JSON:encode(_losList) .. " \n", "127.0.0.1", PANDA.SEND_TO_PORT))
+  log_file = io.open(lfs.writedir() .. [[Logs\Export.log]], "w")
+  log_file:write(string.format("start"))
   
 
-  
-  PANDA.logFile = io.open(lfs.writedir() .. [[Logs\PDebug.log]], "w")
-  
-function PANDA.log(str)
-    if PANDA.logFile then
-        PANDA.logFile:write(str .. "\n")
-        PANDA.logFile:flush()
-    end
-end
 
-
-  PANDA.log("Loaded Test Export.Lua version: 0.0.0.1")
-  
 end
 function LuaExportAfterNextFrame()
 
---  local _GeoPoint = LoGeoCoordinatesToLoCoordinates(_client.lng,_client.lat)
---  PANDA.log(_GeoPoint)
 
-  local IAS = LoGetIndicatedAirSpeed()
-  PANDA.log(IAS)
 
 end
   
-function LuaExportActivityNextEvent()
+function LuaExportActivityNextEvent(t)
 
+local tNext = t
+
+-- Put your event code here and increase tNext for the next event
+-- so this function will be called automatically at your custom
+-- model times. 
+-- If tNext == t then the activity will be terminated.
+
+  if log_file then
+		local o = LoGetWorldObjects()
+			for k,v in pairs(o) do
+				log_file:write(string.format("t = %.2f, ID = %d, name = %s, country = %s(%s), LatLongAlt = (%f, %f, %f), heading = %f\n", t, k, v.Name, v.Country, v.Coalition, v.LatLongAlt.Lat, v.LatLongAlt.Long, v.LatLongAlt.Alt, v.Heading))
+		end
+		
+			tNext = tNext + 1.0
+			
+			return tNext
 end
 
 function LuaExportStop() 
-  if PANDA.UDPSendSocket then
-    PANDA.UDPSendSocket:close()
-  end
-end
+
   
+    log_file:write(string.format("exit"))
+	
+	if log_file then
+	log_file:close()
+	log_file = nil
+	end
+end
+ end 
