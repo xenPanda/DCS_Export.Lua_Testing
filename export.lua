585f8939d23c7c97f2519b20fc2123cf646083b7
Panda <xenpanda@protonmail.com>
2020-04-04 00:42:32 -0700

    working udp socket

--- export.lua
+++ export.lua
@@ -3,14 +3,19 @@ local log_file = nil
 function LuaExportStart()
 
 
-  package.path = package.path..";"..lfs.currentdir().."/LuaSocket/?.lua"
-  package.cpath = package.cpath..";"..lfs.currentdir().."/LuaSocket/?.dll"
+	package.path  = package.path..";"..lfs.currentdir().."/LuaSocket/?.lua"
+	package.cpath = package.cpath..";"..lfs.currentdir().."/LuaSocket/?.dll"
+
+	socket = require("socket")
+	IPAddress = "127.0.0.1"
+	Port = 31090
+
+	MySocket = socket.try(socket.connect(IPAddress, Port))
+	MySocket:setoption("tcp-nodelay",true) 
 
 
   log_file = io.open(lfs.writedir() .. [[Logs\Export.log]], "w")
   log_file:write(string.format("start"))
-  
-
 
 end
 function LuaExportAfterNextFrame()
@@ -28,6 +33,7 @@ local tNext = t
 -- model times. 
 -- If tNext == t then the activity will be terminated.
 
+--[[
   if log_file then
 		local o = LoGetWorldObjects()
 			for k,v in pairs(o) do
@@ -35,8 +41,17 @@ local tNext = t
 		end
 		
 			tNext = tNext + 1.0
-			
-			return tNext
+]]--	
+		
+		local o = LoGetWorldObjects()
+			for k,v in pairs(o) do				
+				socket.try(MySocket:send(string.format("ID = %d, Name = %s, Coalition = %s, Alt = %.0f, Heading = %f, Lat= %f, Long = %f \n", k, v.Name, v.Coalition, v.LatLongAlt.Alt, v.Heading, v.LatLongAlt.Lat, v.LatLongAlt.Long)))
+			end
+		
+		
+		tNext = tNext + 1.0
+	
+		return tNext
 end
 
 function LuaExportStop() 
@@ -48,5 +63,10 @@ function LuaExportStop()
 	log_file:close()
 	log_file = nil
 	end
+	
+	if MySocket then 
+		socket.try(MySocket:send("quit")) -- to close the listener socket
+		MySocket:close()
+	end
 end
- end 
+ 
